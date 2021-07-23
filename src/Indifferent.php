61efdcff419a78897249c8579ac0210f5760050d
Max Goryunov <maximthehedgehog@yandex.ru>
2021-07-23 17:03:54 +0300

    #7: removed fakes/ from Infection check

--- src/Indifferent.php
+++ src/Indifferent.php
@@ -6,6 +6,9 @@ namespace MaxGoryunov\SavingIterator\Src;
  * Interface for objects on which any methods can be called.
  * @template T of object
  * @mixin T
+ * 
+ * @todo #7:10min Add fakes directory back to infection.json.dist after fakes
+ *  folder will be created
  */
 interface Indifferent
 {
