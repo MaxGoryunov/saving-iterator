a998daa276bf159fff978a13da46b7b80500b52c
Max Goryunov <maximthehedgehog@yandex.ru>
2021-07-25 23:03:22 +0300

    #34: added Let and The fakes classes

---
+++ fakes/The.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace MaxGoryunov\SavingIterator\Fakes;
+
+use Closure;
+
+/**
+ * Class for applying contexts to elements without changing them.
+ */
+class The
+{
+
+    /**
+     * Ctor.
+     * 
+     * @param mixed   $subject
+     * @param Closure $context
+     */
+    public function __construct(
+        /**
+         * Element to be put into the context.
+         * 
+         * @var mixed
+         */
+        private mixed $subject,
+
+        /**
+         * Context for the element.
+         * 
+         * @var Closure
+         */
+        private Closure $context
+    ) {
+    }
+
+    /**
+     * Applies context to subject and returns subject.
+     *
+     * @return mixed
+     */
+    public function value(): array
+    {
+        ($this->context)($this->subject);
+        return $this->subject;
+    }
+}
---
+++ tests/fakes/TheTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MaxGoryunov\SavingIterator\Tests\Fakes;
+
+use MaxGoryunov\SavingIterator\Fakes\Let;
+use MaxGoryunov\SavingIterator\Fakes\The;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass MaxGoryunov\SavingIterator\Fakes\The
+ */
+class TheTest extends TestCase
+{
+
+    /**
+     * @covers ::__construct
+     * @covers ::value
+     * 
+     * @small
+     *
+     * @return void
+     */
+    public function testReturnsSubjectsAfterApplyingContexts(): void
+    {
+        $this->assertEquals(
+            ...(new Let(
+                [1, 2, 3, 4, 5],
+                fn(array $nums): array => [
+                    $nums,
+                    (new The(
+                        $nums,
+                        fn(array $nums): array => [
+                            array_sum($nums), array_product($nums)
+                        ]
+                    ))->value()
+                ]
+            ))->value()
+        );
+    }
+}
