5f41f7703b5e70b757370362c79a91070743f677
Max Goryunov <maximthehedgehog@yandex.ru>
2021-08-22 23:27:27 +0300

    #66: added test suite

--- tests/src/ArrayAddingIteratorTest.php
+++ tests/src/ArrayAddingIteratorTest.php
@@ -16,6 +16,8 @@ final class ArrayAddingIteratorTest extends TestCase
      * @covers ::__construct
      * @covers ::from
      * @covers ::current
+     * 
+     * @small
      *
      * @return void
      */
@@ -27,6 +29,37 @@ final class ArrayAddingIteratorTest extends TestCase
             (new ArrayAddingIterator())
                 ->from($origin)
                 ->current()
+        );
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::from
+     * @covers ::current
+     * @covers ::key
+     * @covers ::valid
+     * @covers ::rewind
+     * @covers ::next
+     * 
+     * @small
+     *
+     * @return void
+     */
+    public function testWorksAsIteratorWithAddedValues(): void
+    {
+        /**
+         * @todo #66:40min Add a fake class for putting the values into Adding
+         *  Iterator from source.
+         */
+        $origin   = new ArrayIterator([8, 20, 5, 1, 65, 2, 6]);
+        $iterator = (new ArrayAddingIterator())->from($origin);
+        for ($i = 0; $i < $origin->count() - 1; $i++) {
+            $origin->next();
+            $iterator = $iterator->from($origin);
+        }
+        $this->assertEquals(
+            iterator_to_array($origin),
+            iterator_to_array($iterator)
         );
     }
 }
