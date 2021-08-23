dc83a0be3a87964b567d2e0bfdfd87de2c2b29ba
Max Goryunov <maximthehedgehog@yandex.ru>
2021-08-24 02:25:57 +0300

    #82: added IteratorTransfer

---
+++ fakes/IteratorTransfer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MaxGoryunov\SavingIterator\Fakes;
+
+use Iterator;
+use MaxGoryunov\SavingIterator\Src\AddingIterator;
+
+/**
+ * Performs transfer of values from iterator to adding iterator.
+ */
+final class IteratorTransfer
+{
+
+    /**
+     * Ctor.
+     * 
+     * @param Iterator $origin original iterator.
+     */
+    public function __construct(
+        /**
+         * Original iterator.
+         *
+         * @var Iterator
+         */
+        private Iterator $origin
+    ) {
+    }
+
+    /**
+     * Transfers all values from origin to target.
+     *
+     * @param AddingIterator $target
+     * @return AddingIterator
+     */
+    public function toTarget(AddingIterator $target): AddingIterator
+    {
+        while ($this->origin->valid()) {
+            $target = $target->from($this->origin);
+            $this->origin->next();
+        }
+        return $target;
+    }
+}
---
+++ tests/fakes/IteratorTransferTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MaxGoryunov\SavingIterator\Tests\Fakes;
+
+use ArrayIterator;
+use MaxGoryunov\SavingIterator\Fakes\IteratorTransfer;
+use MaxGoryunov\SavingIterator\Src\ArrayAddingIterator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass MaxGoryunov\SavingIterator\Fakes\IteratorTransfer
+ */
+final class IteratorTransferTest extends TestCase
+{
+
+    /**
+     * @covers ::__construct
+     * 
+     * @small
+     *
+     * @return void
+     */
+    public function testTransfersValuesFromOriginToTarget(): void
+    {
+        $input = [4, 65, 2, 76, 23, 8, 82, 6,2, 8];
+        $this->assertEquals(
+            $input,
+            iterator_to_array(
+                (new IteratorTransfer(
+                    new ArrayIterator($input)
+                ))
+                    ->toTarget(new ArrayAddingIterator())
+            )
+        );
+    }
+}
