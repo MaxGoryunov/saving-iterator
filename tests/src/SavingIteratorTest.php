ae8051a2b65d8add05598c41efe547671f07d01b
Max Goryunov <maximthehedgehog@yandex.ru>
2021-08-29 23:55:19 +0300

    #84: fixed tests

--- tests/src/SavingIteratorTest.php
+++ tests/src/SavingIteratorTest.php
@@ -13,6 +13,7 @@ use MaxGoryunov\SavingIterator\Src\Indifferent;
 use MaxGoryunov\SavingIterator\Src\TimesCalled;
 use MaxGoryunov\SavingIterator\Src\TransparentIterator;
 use MaxGoryunov\SavingIterator\Src\SavingIterator;
+use MaxGoryunov\SavingIterator\Src\ValidAddingIterator;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -47,7 +48,9 @@ class SavingIteratorTest extends TestCase
                 iterator_to_array(
                     new SavingIterator(
                         new ArrayIterator($nums),
-                        new ArrayAddingIterator()
+                        new ValidAddingIterator(
+                            new ArrayAddingIterator()
+                        )
                     )
                 )
             )
@@ -88,7 +91,9 @@ class SavingIteratorTest extends TestCase
                                 new SavingIterator(
                                     /** @phpstan-ignore-next-line */
                                     new TransparentIterator($called),
-                                    new ArrayAddingIterator()
+                                    new ValidAddingIterator(
+                                        new ArrayAddingIterator()
+                                    )
                                 )
                             ),
                             0,
@@ -131,7 +136,9 @@ class SavingIteratorTest extends TestCase
                                 yield $i;
                             }
                         })(),
-                        new ArrayAddingIterator()
+                        new ValidAddingIterator(
+                            new ArrayAddingIterator()
+                        )
                     )
                 )
             )
@@ -165,7 +172,9 @@ class SavingIteratorTest extends TestCase
                         yield $i;
                     }
                 })(),
-                new ArrayAddingIterator()
+                new ValidAddingIterator(
+                    new ArrayAddingIterator()
+                )
             ),
             fn(Iterator $iterator) => $this->assertEquals(
                 iterator_to_array($iterator),
@@ -200,7 +209,9 @@ class SavingIteratorTest extends TestCase
                             yield $value;
                         }
                     })(),
-                    new ArrayAddingIterator()
+                    new ValidAddingIterator(
+                        new ArrayAddingIterator()
+                    )
                 )
             )
         );
@@ -228,7 +239,9 @@ class SavingIteratorTest extends TestCase
         (new The(
             new SavingIterator(
                 new ArrayIterator([1, 15, 73, 234, 65, 23, 71, 76, 9, 23]),
-                new ArrayAddingIterator()
+                new ValidAddingIterator(
+                    new ArrayAddingIterator()
+                )
             ),
             fn(Iterator $iterator) => $this->assertEquals(
                 iterator_to_array($iterator),
@@ -264,7 +277,9 @@ class SavingIteratorTest extends TestCase
                     (new The(
                         new SavingIterator(
                             new ArrayIterator($input),
-                            new ArrayAddingIterator()
+                            new ValidAddingIterator(
+                                new ArrayAddingIterator()
+                            )
                         ),
                         function (Iterator $iterator) use ($input): void {
                             foreach ($iterator as $value) {
@@ -301,7 +316,9 @@ class SavingIteratorTest extends TestCase
             iterator_to_array(
                 new SavingIterator(
                     new ArrayIterator([]),
-                    new ArrayAddingIterator()
+                    new ValidAddingIterator(
+                        new ArrayAddingIterator()
+                    )
                 )
             )
         );
@@ -343,7 +360,9 @@ class SavingIteratorTest extends TestCase
                                 new SavingIterator(
                                     /** @phpstan-ignore-next-line */
                                     new TransparentIterator($called),
-                                    new ArrayAddingIterator()
+                                    new ValidAddingIterator(
+                                        new ArrayAddingIterator()
+                                    )
                                 )
                             ),
                             0,
